c8a9f593136e3dee20051518bbfa8144b1b5c05d
Gilang Pratama <pratamapriadi96@gmail.com>
2022-01-28 18:05:15 +0700

    :wrench: Update connection_util.py

--- utils/connection_util.py
+++ utils/connection_util.py
@@ -1,13 +1,6 @@
-import os
 import motor.motor_asyncio
-from os.path import join
-from dotenv import load_dotenv
-
-# Load dotenv
-dotenv_path = join('./', '.env')
-load_dotenv(dotenv_path)
 
 # Database setting
-mongodb_uri = os.environ.get("MONGODB_URI")
+mongodb_uri = "mongodb://localhost:27017"
 client = motor.motor_asyncio.AsyncIOMotorClient(mongodb_uri)
 database = client.fastapi
